318be1833580602c7ceeaf567ff2465e856fa9f8
Dok1211 <wataru.douke@gmail.com>
2017-09-11 11:50:46 +0900

    create add

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -21,4 +21,18 @@ class TodoController extends Controller
     	$todos = $this->todo->all();
     	return view('todo.index',compact('todos'));
     }
+
+    public function create()
+    {
+    	return view('todo.create');
+    }
+
+    public function store(request $request)
+    {
+    	$input = $request->all();
+    	$this->todo->fill($input);
+    	$this->todo->save();
+
+    	return redirect()->to('todo');
+    }
 }
